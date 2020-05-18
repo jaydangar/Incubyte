5a81294cf8141fe84213813539f36982d7e5ed4f
JAY DANGAR <jayjaydangar96@gmail.com>
2020-05-18 21:11:37 +0530

    LeapYearChecker Code added without Exception Handling

---
+++ LeapYear/LeapYearChecker.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class LeapYearChecker{
+    public static void main(String[] args) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+
+        while(true){
+
+            String strYear = bufferedReader.readLine();
+            if(strYear==null){
+                bufferedReader.close();
+                return;
+            }
+
+            int year = Integer.parseInt(strYear);
+            String leapYearPrint = " is a Leap Year.";
+            
+            if(!isLeapYear(year)){
+                leapYearPrint = " is not a Leap Year.";
+            }
+
+            System.out.println(year + leapYearPrint);
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+        if(year%4!=0){
+            return false;
+        }
+
+        if(year%100==0 && year%400!=0){
+            return false;
+        }
+
+        return true;
+    }
+}
